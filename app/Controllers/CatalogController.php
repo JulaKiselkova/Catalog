ce3f6111d11a5c5b8c2e8c7e93df5ef8fa32f175
JulaKiselkova <Yulia.kiselkova@mail.ru>
2021-11-29 13:13:00 +0300

    No hometask commit

--- app/Controllers/CatalogController.php
+++ app/Controllers/CatalogController.php
@@ -6,28 +6,24 @@ use App\Models\Product;
 
 class CatalogController
 {
-    public function index()
-    {
+    public function index() {
         render('catalog.php');
     }
 
-    public function showProduct()
-    {
-        $id = (int)$_GET['id'];
+    public function showProduct() {
+        $id = (int) $_GET['id'];
         $product = Product::findById($id);
         var_dump($product);
         render('catalog12.php', ['product' => $product]);
     }
 
-    public function showForm()
-    {
+    public  function showForm() {
         render('addProductForm.php');
     }
 
-    public function saveProduct()
-    {
+    public  function saveProduct() {
         print_r($_FILES['img']);
-        $path = $_SERVER['DOCUMENT_ROOT'] . '/' . $_FILES['img']['name'];
+        $path = $_SERVER['DOCUMENT_ROOT'].'/'.$_FILES['img']['name'];
         //copy($_FILES['img']['tmp_name'], __DIR__.'/1.png');
         copy($_FILES['img']['tmp_name'], $path);
         move_uploaded_file($_FILES['img']['name'], $path);
