7b1a2e6cbdaa94ab21511dcfaeedd4c967c60817
JulaKiselkova <Yulia.kiselkova@mail.ru>
2021-11-29 12:14:53 +0300

    Second commit

--- viwes/form_homeTask.php
+++
@@ -1,6 +0,0 @@
-<form action="/save_homeTask" method="post" enctype="multipart/form-data">
-    <input type="text" name="name">
-    <input type="file" name="img[]" multiple>
-    <textarea name = "description"></textarea>
-    <button type="submit">Send</button>
-</form>>
