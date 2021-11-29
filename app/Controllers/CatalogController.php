5567b1b7e863671696c96a9e5f1d28ba0bee351c
Yulia Kiselkova <78469979+JulaKiselkova@users.noreply.github.com>
2021-11-29 13:25:32 +0300

    Update CatalogController.php
    change syntax

--- app/Controllers/CatalogController.php
+++ app/Controllers/CatalogController.php
@@ -6,26 +6,22 @@ use App\Models\Product;
 
 class CatalogController
 {
-    public function index()
-    {
+    public function index() {
         render('catalog.php');
     }
 
-    public function showProduct()
-    {
+    public function showProduct() {
         $id = (int)$_GET['id'];
         $product = Product::findById($id);
         var_dump($product);
         render('catalog12.php', ['product' => $product]);
     }
 
-    public function showForm()
-    {
+    public function showForm() {
         render('addProductForm.php');
     }
 
-    public function saveProduct()
-    {
+    public function saveProduct() {
         print_r($_FILES['img']);
         $path = $_SERVER['DOCUMENT_ROOT'] . '/' . $_FILES['img']['name'];
         //copy($_FILES['img']['tmp_name'], __DIR__.'/1.png');
